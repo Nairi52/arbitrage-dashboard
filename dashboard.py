4e5a8d1144df0fd2d6c2250e35ee2a0295e3411e
Nairi52 <rayanmodou52@hotmail.fr>
2025-07-25 22:11:43 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -38,7 +38,8 @@ TOKEN_MINTS = {
 
 JUPITER_API_URL = "https://quote-api.jup.ag/v6/quote"
 
-async def get_price(session, token_in, token_out, amount=1_000_000):if token_out not in TOKEN_MINTS or token_in not in TOKEN_MINTS:
+async def get_price(session, token_in, token_out, amount=1_000_000):
+    if token_out not in TOKEN_MINTS or token_in not in TOKEN_MINTS:
     return None
  params = {
         "inputMint": TOKEN_MINTS[token_in],
